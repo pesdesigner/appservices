543897e3a4aaa0961afa748ba7831f93a4cb3112
pesdesigner <pauloeduardo.dg@outlook.com>
2020-11-04 23:33:40 -0300

    Service layer, component registration

---
+++ src/main/java/com/graficoeweb/appservices/services/UserService.java
@@ -0,0 +1,26 @@
+package com.graficoeweb.appservices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.graficoeweb.appservices.entities.User;
+import com.graficoeweb.appservices.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired		
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
