d7b2bfb2c2a26fc1845685e56fd083dbf432bc66
pesdesigner <pauloeduardo.dg@outlook.com>
2020-11-04 21:09:27 -0300

    Project created

---
+++ src/main/java/com/graficoeweb/appservices/AppservicesApplication.java
@@ -0,0 +1,13 @@
+package com.graficoeweb.appservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppservicesApplication.class, args);
+	}
+
+}
