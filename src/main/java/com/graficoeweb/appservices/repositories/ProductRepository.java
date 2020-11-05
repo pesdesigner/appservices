14686d08161caefb7679fd72eb56fa7da4df366c
pesdesigner <pauloeduardo.dg@outlook.com>
2020-11-05 19:23:40 -0300

    Product

---
+++ src/main/java/com/graficoeweb/appservices/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.graficoeweb.appservices.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.graficoeweb.appservices.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
