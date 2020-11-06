0b231d815d7859a06eff80937a0bff442423ed14
pesdesigner <pauloeduardo.dg@outlook.com>
2020-11-06 18:41:35 -0300

    Exception handling - findById
    * NEW CLASS: services.exceptions.ResourceNotFoundException
    * NEW CLASS: resources.exceptions.StandardError
    * NEW CLASS: resources.exceptions.ResourceExceptionHandler
    * UserService

--- src/main/java/com/graficoeweb/appservices/services/UserService.java
+++ src/main/java/com/graficoeweb/appservices/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.graficoeweb.appservices.entities.User;
 import com.graficoeweb.appservices.repositories.UserRepository;
+import com.graficoeweb.appservices.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
