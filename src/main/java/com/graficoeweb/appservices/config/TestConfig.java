67eb20a133533e2d9cf4467963820d94209f3730
pesdesigner <pauloeduardo.dg@outlook.com>
2020-11-06 00:09:05 -0300

    Payment, one-to-one association

--- src/main/java/com/graficoeweb/appservices/config/TestConfig.java
+++ src/main/java/com/graficoeweb/appservices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.graficoeweb.appservices.entities.Category;
 import com.graficoeweb.appservices.entities.Order;
 import com.graficoeweb.appservices.entities.OrderItem;
+import com.graficoeweb.appservices.entities.Payment;
 import com.graficoeweb.appservices.entities.Product;
 import com.graficoeweb.appservices.entities.User;
 import com.graficoeweb.appservices.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}	
 }
