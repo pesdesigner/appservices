a5ac5c4ebcb3b7ea8e2bc0e1721cccfef6b6daf4
pesdesigner <pauloeduardo.dg@outlook.com>
2020-11-05 23:17:00 -0300

    OrderItem, many-to-many association with extra attributes - PART 2

--- src/main/java/com/graficoeweb/appservices/config/TestConfig.java
+++ src/main/java/com/graficoeweb/appservices/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.graficoeweb.appservices.entities.Category;
 import com.graficoeweb.appservices.entities.Order;
+import com.graficoeweb.appservices.entities.OrderItem;
 import com.graficoeweb.appservices.entities.Product;
 import com.graficoeweb.appservices.entities.User;
 import com.graficoeweb.appservices.entities.enums.OrderStatus;
 import com.graficoeweb.appservices.repositories.CategoryRepository;
+import com.graficoeweb.appservices.repositories.OrderItemRepository;
 import com.graficoeweb.appservices.repositories.OrderRepository;
 import com.graficoeweb.appservices.repositories.ProductRepository;
 import com.graficoeweb.appservices.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,7 +73,13 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
-	}
-	
+	}	
 }
